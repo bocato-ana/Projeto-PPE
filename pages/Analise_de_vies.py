0a0250d04acec52f4ee875476017026599d073d5
bocato-ana <analuizabocatodasilva@gmail.com>
2026-03-30 16:48:51 -0300

    Update Analise_de_vies.py

--- pages/Analise_de_vies.py
+++ pages/Analise_de_vies.py
@@ -27,12 +27,18 @@ if 'data' in st.session_state:
 
     # Ordens Lógicas
     ordem_salario = [
-        'Menos de R$ 1.000/mês', 'de R$ 1.001/mês a R$ 2.000/mês', 
-        'de R$ 2.001/mês a R$ 3.000/mês', 'de R$ 3.001/mês a R$ 4.000/mês',
-        'de R$ 4.001/mês a R$ 6.000/mês', 'de R$ 6.001/mês a R$ 8.000/mês',
-        'de R$ 8.001/mês a R$ 12.000/mês', 'de R$ 12.001/mês a R$ 16.000/mês',
-        'de R$ 16.001/mês a R$ 20.000/mês', 'de R$ 20.001/mês a R$ 25.000/mês',
-        'de R$ 25.001/mês a R$ 30.000/mês', 'de R$ 30.001/mês a R$ 40.000/mês',
+        'Menos de R$ 1.000/mês', 
+        'de R$ 1.001/mês a R$ 2.000/mês', 
+        'de R$ 2.001/mês a R$ 3.000/mês', 
+        'de R$ 3.001/mês a R$ 4.000/mês',
+        'de R$ 4.001/mês a R$ 6.000/mês', 
+        'de R$ 6.001/mês a R$ 8.000/mês',
+        'de R$ 8.001/mês a R$ 12.000/mês', 
+        'de R$ 12.001/mês a R$ 16.000/mês',
+        'de R$ 16.001/mês a R$ 20.000/mês', 
+        'de R$ 20.001/mês a R$ 25.000/mês',
+        'de R$ 25.001/mês a R$ 30.000/mês', 
+        'de R$ 30.001/mês a R$ 40.000/mês',
         'Acima de R$ 40.001/mês'
     ]
     ordem_nivel = ['Júnior', 'Pleno', 'Sênior', 'Gestão']
