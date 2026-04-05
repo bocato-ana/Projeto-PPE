c32d0344751525976d414e8b0625312604daca07
bocato-ana <analuizabocatodasilva@gmail.com>
2026-04-05 13:59:19 -0300

    adicionais

---
+++ pages/Estudo_IA.py
@@ -0,0 +1,4 @@
+st.title('Estudo e Análise de Viès com IA')
+st.subheader('Estudo realizado através de regressão lógica e árvore de decisão')
+
+st.write("""Todo o estudo se encontra no repositório do GitHub(notebooks)""")
